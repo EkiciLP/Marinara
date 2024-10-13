ac8f6bdbb3f82d01225e4ba53f600bd879f5e9e2
tueem <tueem@tomatentum.net>
2024-10-14 00:54:50 +0200

    First basic data in API object

--- lib/src/main/java/net/tomatentum/marinara/Marinara.java
+++ lib/src/main/java/net/tomatentum/marinara/Marinara.java
@@ -1,7 +1,22 @@
 package net.tomatentum.marinara;
 
+import net.tomatentum.marinara.registry.InteractionRegistry;
+import net.tomatentum.marinara.wrapper.LibraryWrapper;
+
 public class Marinara {
-    public static Marinara load() {
-        
+    
+    public static Marinara load(LibraryWrapper wrapper) {
+        InteractionRegistry registry = new InteractionRegistry(wrapper);
+        return new Marinara(registry);
+    }
+
+    private InteractionRegistry registry;
+
+    private Marinara(InteractionRegistry registry) {
+        this.registry = registry;
+    }
+
+    public InteractionRegistry getRegistry() {
+        return registry;
     }
 }
