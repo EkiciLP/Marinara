e57f29500be2728fd5950b96a5541fab571b2d5e
tueem <tueem@tomatentum.net>
2024-10-14 00:17:16 +0200

    rename Root- to ApplicationCommand

--- lib/src/main/java/net/tomatentum/marinara/interaction/commands/annotation/ApplicationCommand.java
+++ lib/src/main/java/net/tomatentum/marinara/interaction/commands/annotation/ApplicationCommand.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RootCommand {
+public @interface ApplicationCommand {
     public String name();
     public String description() default "";
     public String[] aliases() default {};
