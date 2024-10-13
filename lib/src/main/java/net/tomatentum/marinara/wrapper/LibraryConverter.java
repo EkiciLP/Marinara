ddeed7c0dcc7e1391fa1b097517e01328ebf1c2d
tueem <tueem@tomatentum.net>
2024-10-13 21:15:30 +0200

    remove converter

--- lib/src/main/java/net/tomatentum/marinara/wrapper/LibraryConverter.java
+++
@@ -1,9 +0,0 @@
-package net.tomatentum.marinara.wrapper;
-
-public interface LibraryConverter {
-    public Object toAttachment(Object context, String option);
-    public Object toChannel(Object context, String option);
-    public Object toMentionable(Object context, String option);
-    public Object toRole(Object context, String option);
-    public Object toUser(Object context, String option);
-}
