492399ec825ee21076174ac6b94099947ba5400a
tueem <tueem@tomatentum.net>
2024-10-25 19:40:50 +0200

    Switch to internal Librarywrapper instance management.

--- lib/src/main/java/net/tomatentum/marinara/Marinara.java
+++ lib/src/main/java/net/tomatentum/marinara/Marinara.java
@@ -1,13 +1,24 @@
 package net.tomatentum.marinara;
 
+import java.lang.reflect.Constructor;
+
 import net.tomatentum.marinara.registry.InteractionRegistry;
 import net.tomatentum.marinara.wrapper.LibraryWrapper;
 
 public class Marinara {
     
-    public static Marinara load(LibraryWrapper wrapper) {
-        InteractionRegistry registry = new InteractionRegistry(wrapper);
-        return new Marinara(registry);
+    public static <T extends LibraryWrapper> Marinara load(Class<T> clazz) {
+        try {
+            Constructor<T> ctor = clazz.getConstructor();
+            ctor.setAccessible(true);
+            T wrapper = ctor.newInstance();
+            InteractionRegistry registry = new InteractionRegistry(wrapper);
+            return new Marinara(registry);
+        }catch (Exception ex) {
+            System.err.println(ex);
+            System.exit(100);
+            return null;
+        }
     }
 
     private InteractionRegistry registry;
