cf45d0e8ea920545646238d8eeaaaf2381816946
tueem <tueem@tomatentum.net>
2024-10-25 20:47:54 +0200

    remove unused import

--- lib/src/main/java/net/tomatentum/marinara/registry/InteractionRegistry.java
+++ lib/src/main/java/net/tomatentum/marinara/registry/InteractionRegistry.java
@@ -2,7 +2,6 @@ package net.tomatentum.marinara.registry;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
