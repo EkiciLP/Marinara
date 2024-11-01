9d8152242968159395d23812c8146ba791199be4
tueem <tueem@tomatentum.net>
2024-11-01 16:07:10 +0100

    switch to user create wrapper instances

--- lib/src/main/java/net/tomatentum/marinara/Marinara.java
+++ lib/src/main/java/net/tomatentum/marinara/Marinara.java
@@ -1,24 +1,13 @@
 package net.tomatentum.marinara;
 
-import java.lang.reflect.Constructor;
-
 import net.tomatentum.marinara.registry.InteractionRegistry;
 import net.tomatentum.marinara.wrapper.LibraryWrapper;
 
 public class Marinara {
     
-    public static <T extends LibraryWrapper> Marinara load(Class<T> clazz) {
-        try {
-            Constructor<T> ctor = clazz.getConstructor();
-            ctor.setAccessible(true);
-            T wrapper = ctor.newInstance();
-            InteractionRegistry registry = new InteractionRegistry(wrapper);
-            return new Marinara(registry);
-        }catch (Exception ex) {
-            System.err.println(ex);
-            System.exit(100);
-            return null;
-        }
+    public static <T extends LibraryWrapper> Marinara load(LibraryWrapper wrapper) {
+        InteractionRegistry registry = new InteractionRegistry(wrapper);
+        return new Marinara(registry);
     }
 
     private InteractionRegistry registry;
