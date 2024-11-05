0ce0b3eb4fd7b312df159a532bb4de04ea78c83e
tueem <tueem@tomatentum.net>
2024-11-05 15:54:49 +0100

    Add Button Interaction with specific method parameter support.

---
+++ lib/src/main/java/net/tomatentum/marinara/interaction/annotation/Button.java
@@ -0,0 +1,5 @@
+package net.tomatentum.marinara.interaction.annotation;
+
+public @interface Button {
+    public String customId();
+}
--- lib/src/main/java/net/tomatentum/marinara/wrapper/LibraryWrapper.java
+++ lib/src/main/java/net/tomatentum/marinara/wrapper/LibraryWrapper.java
@@ -17,7 +17,6 @@ public abstract class LibraryWrapper {
         interactionSubscriber = new ArrayList<>();
     }
 
-    public abstract void registerSlashCommands(SlashCommandDefinition[] defs); 
 
     public void handleInteraction(Object context) {
         interactionSubscriber.forEach((o) -> o.accept(context));
@@ -31,6 +30,11 @@ public abstract class LibraryWrapper {
     }
 
     public abstract InteractionType getInteractionType(Class<?> clazz);
+
+    public abstract void registerSlashCommands(SlashCommandDefinition[] defs); 
     public abstract Object convertCommandOption(Object context, SlashCommandOptionType type, String optionName);
     public abstract ExecutableSlashCommandDefinition getCommandDefinition(Object context);
+
+    public abstract String getButtonId(Object context);
+    public abstract Object getComponentContextObject(Object context, Class<?> type);
 }
