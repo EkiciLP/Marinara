ca0ab13316a097a77f41536e6b155f08c643be49
tueem <tueem@tomatentum.net>
2025-02-19 14:10:45 +0100

    migrate ButtonTest to mockito

--- wrapper/javacord/src/test/java/net/tomatentum/marinara/test/javacord/ButtonTest.java
+++ wrapper/javacord/src/test/java/net/tomatentum/marinara/test/javacord/ButtonTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import net.tomatentum.marinara.Marinara;
-import net.tomatentum.marinara.test.javacord.mocks.ButtonInteractionMock;
-import net.tomatentum.marinara.test.javacord.mocks.DiscordApiMock;
+import net.tomatentum.marinara.test.javacord.mocks.CommonMocks;
 import net.tomatentum.marinara.wrapper.LibraryWrapper;
 import net.tomatentum.marinara.wrapper.javacord.JavacordWrapper;
 
@@ -17,10 +16,10 @@ public class ButtonTest {
 
     @Test
     public void testButtonExecution() {
-        LibraryWrapper wrapper = new JavacordWrapper(new DiscordApiMock()); //null okay as we don't use the discord API in this test.
+        LibraryWrapper wrapper = new JavacordWrapper(null); //null okay as we don't use the discord API in this test.
         Marinara marinara = Marinara.load(wrapper);
         marinara.getRegistry().addInteractions(new TestButton());
-        wrapper.handleInteraction(new ButtonInteractionMock("test"));
+        wrapper.handleInteraction(CommonMocks.getButtonInteractionMock("test"));
         assertTrue(TestButton.didRun);
     }
     
---
+++ wrapper/javacord/src/test/java/net/tomatentum/marinara/test/javacord/mocks/CommonMocks.java
@@ -0,0 +1,29 @@
+package net.tomatentum.marinara.test.javacord.mocks;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.interaction.ButtonInteraction;
+
+public class CommonMocks {
+
+    public static ButtonInteraction getButtonInteractionMock(String customId) {
+        ButtonInteraction buttonInteractionMock = mock();
+        
+        when(buttonInteractionMock.getCustomId()).thenReturn(customId);
+        when(buttonInteractionMock.getMessage()).thenReturn(mock(Message.class));
+        when(buttonInteractionMock.getServer()).thenReturn(Optional.of(mock(Server.class)));
+        when(buttonInteractionMock.getChannel()).thenReturn(Optional.of(mock(TextChannel.class)));
+        when(buttonInteractionMock.getUser()).thenReturn(mock(User.class));
+
+
+        return buttonInteractionMock;
+    }
+    
+}
