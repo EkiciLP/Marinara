f6db113deba8a90f70812613b2bd72d21e3db1ab
Tueem <tueem@tomatentum.net>
2025-02-20 00:41:15 +0100

    add CommandConverter stub

---
+++ lib/src/main/java/net/tomatentum/marinara/interaction/commands/CommandConverter.java
@@ -0,0 +1,23 @@
+package net.tomatentum.marinara.interaction.commands;
+
+import net.tomatentum.marinara.interaction.commands.annotation.SlashCommandOption;
+import net.tomatentum.marinara.interaction.commands.annotation.SubCommand;
+import net.tomatentum.marinara.interaction.commands.annotation.SubCommandGroup;
+
+public class CommandConverter<A extends Object> {
+
+    private Spec<A, ?, ?> spec;
+    
+    public A convert(SlashCommandDefinition def) {
+        return null;
+    }
+
+    public interface Spec<A extends Object, O extends Object, C extends Object> {
+
+        public A convertCommand(SlashCommandDefinition def);
+        public O convertSubCommand(SubCommand def, O[] options);
+        public O convertSubCommandGroup(SubCommandGroup def, O[] options);
+        public O convertOption(SlashCommandOption option, C[] choices);
+        public C[] convertChoice(SlashCommandOption option);
+    }
+}
