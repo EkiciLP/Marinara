78cacb7eb6ecb271ae4bda04fb6be7b4dc6eeb8b
tueem <tueem@tomatentum.net>
2025-03-16 02:47:16 +0100

    feat(util): add ObjectListAggregator

---
+++ lib/src/main/java/net/tomatentum/marinara/util/ObjectListAggregator.java
@@ -0,0 +1,15 @@
+package net.tomatentum.marinara.util;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
+public class ObjectListAggregator<O, K, V> extends ObjectAggregator<O, K, ArrayList<V>> {
+
+    public ObjectListAggregator(Function<O, K> keySupplier, Function<O, V> valueConsumer) {
+        super(keySupplier, 
+        (l, o) -> l.add(valueConsumer.apply(o)),
+        () -> new ArrayList<>());
+    }
+
+    
+}
