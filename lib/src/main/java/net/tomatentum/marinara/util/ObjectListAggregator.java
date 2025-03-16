e3fc10a1ce17e34c53b1feb25a6a8ae10fca2e1e
tueem <tueem@tomatentum.net>
2025-03-16 02:48:24 +0100

    fix(util): ObjectListAggregator syntax

--- lib/src/main/java/net/tomatentum/marinara/util/ObjectListAggregator.java
+++ lib/src/main/java/net/tomatentum/marinara/util/ObjectListAggregator.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 
 public class ObjectListAggregator<O, K, V> extends ObjectAggregator<O, K, ArrayList<V>> {
 
-    public ObjectListAggregator(Function<O, K> keySupplier, Function<O, V> valueConsumer) {
+    public ObjectListAggregator(Function<O, Iterable<K>> keySupplier, Function<O, V> valueConsumer) {
         super(keySupplier, 
         (l, o) -> l.add(valueConsumer.apply(o)),
         () -> new ArrayList<>());
